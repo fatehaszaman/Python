88f3bf22d18373612a47b3348dffa5fadd7100da
fatehaszaman <168937738+fatehaszaman@users.noreply.github.com>
2025-08-30 19:13:20 -0400

    Create kth_largest_element_in_an_array_215.py

---
+++ Python/kth_largest_element_in_an_array_215.py
@@ -0,0 +1,12 @@
+# Leetcode question number 215
+import heapq
+
+def findKthLargest(nums, k):
+    min_heap = []
+
+    for num in nums:
+        heapq.heappush(min_heap, num)
+        if len(min_heap) > k:
+            heapq.heappop(min_heap)
+
+    return min_heap[0]
