d6fd607472ba8ae7dbad8cc4fe03127b80d8513a
fatehaszaman <168937738+fatehaszaman@users.noreply.github.com>
2025-08-30 19:00:41 -0400

    Create number_of_islands1_200.py

---
+++ Python/number_of_islands1_200.py
@@ -0,0 +1,29 @@
+#ONLY IF YOU IMPORT FROM COLLECTIONS AND IS FASTER PYTHON VERSION
+from collections import deque
+
+class Solution:
+    def numIslands(self, grid: list[list[str]]) -> int:
+        if not grid:
+            return 0
+
+        m, n = len(grid), len(grid[0])
+        count = 0
+
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    count += 1
+                    grid[i][j] = '0'  # mark as visited
+                    queue = deque([(i, j)])
+
+                    while queue:
+                        r, c = queue.popleft()
+                        for dr, dc in directions:
+                            nr, nc = r + dr, c + dc
+                            if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] == '1':
+                                grid[nr][nc] = '0'
+                                queue.append((nr, nc))
+
+        return count
