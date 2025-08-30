53d5ae2e6eb50c4a744f26eb828cedcbeb43ab59
fatehaszaman <168937738+fatehaszaman@users.noreply.github.com>
2025-08-30 18:50:58 -0400

    Create coin_change_332.py

---
+++ Python/coin_change_332.py
@@ -0,0 +1,14 @@
+from typing import List
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # Initialize dp array, amount + 1 is like infinity (unreachable)
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0  # Base case
+        
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if coin <= i:
+                    dp[i] = min(dp[i], dp[i - coin] + 1)
+        
+        return dp[amount] if dp[amount] != amount + 1 else -1
