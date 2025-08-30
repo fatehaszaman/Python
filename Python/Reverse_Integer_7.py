d49fb591b5ec388210b8d16a86bc4f2c57480dac
fatehaszaman <168937738+fatehaszaman@users.noreply.github.com>
2025-08-30 18:43:35 -0400

    Create Reverse_Integer_7.py

---
+++ Python/Reverse_Integer_7.py
@@ -0,0 +1,14 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        res = 0
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+        
+        while x != 0:
+            digit = x % 10
+            x //= 10
+            # Check overflow before it happens
+            if res > (2**31 - 1) // 10:
+                return 0
+            res = res * 10 + digit       
+        return res * sign
