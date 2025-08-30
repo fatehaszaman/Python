49099df40815265b3003917254c87b95c83a68f7
fatehaszaman <168937738+fatehaszaman@users.noreply.github.com>
2025-08-30 18:57:03 -0400

    Update pow_x_n_50.py

--- Python/pow_x_n_50.py
+++ Python/pow_x_n_50.py
@@ -1,13 +1,11 @@
-public class Solution {
-    public double myPow(double x, int n) {
-        long N = n;
-        if (N < 0) { x = 1 / x; N = -N; }
-        double res = 1;
-        while (N > 0) {
-            if ((N & 1) == 1) res *= x;
-            x *= x;
-            N >>= 1;
-        }
-        return res;
-    }
-}
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        N = n
+        if N < 0:
+            x, N = 1 / x, -N
+        res = 1
+        while N > 0:
+            if N & 1: res *= x
+            x *= x
+            N >>= 1
+        return res
