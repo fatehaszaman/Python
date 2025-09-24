6f04843f585c48c946b3d53244d858f29c437fff
fatehaszaman <168937738+fatehaszaman@users.noreply.github.com>
2025-09-24 16:16:47 -0400

    Create PrintFunction.py

---
+++ Hackerrank/PrintFunction.py
@@ -0,0 +1,13 @@
+# The included code stub will read an integer,n, from STDIN.
+# Without using any string methods, try to print the following:
+# 123.. n
+# Note that "..." represents the consecutive values in between.
+
+
+if __name__ == '__main__':
+    n = int(input())
+    string= ""
+    if 0<n<151:
+        for i in range(1,n+1):
+            string+= str(i)
+        print(string)
