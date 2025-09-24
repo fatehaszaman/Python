c46663f662c3fe8a7e7c118bd2e876fb3d89fa92
fatehaszaman <168937738+fatehaszaman@users.noreply.github.com>
2025-09-24 17:04:39 -0400

    Update string_split.py

--- Basics/string_split.py
+++ Basics/string_split.py
@@ -1,2 +1,6 @@
 string = "Tell me and I forget. Teach me and I remember. Involve me and I learn."
 print(string.split("."))
+
+
+# output is
+# ['Tell me and I forget', ' Teach me and I remember', ' Involve me and I learn', '']
