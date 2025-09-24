3324ca4cd3873bd6ff2933299e949f8113c05017
fatehaszaman <168937738+fatehaszaman@users.noreply.github.com>
2025-09-24 16:48:00 -0400

    Create find_missing_number.py

---
+++ Other/find_missing_number.py
@@ -0,0 +1,7 @@
+# Given an array of n distinct numbers from 0 to n, identify the missing number
+
+def find_missing_number(nums):
+    n = len(nums)
+    expected_sum = n * (n + 1) // 2
+    actual_sum = sum(nums)
+    return expected_sum - actual_sum
